99058f6215946b7e5798fb41c357627898497b15
Christopher Nielson <cnielson3@gmail.com>
2022-10-11 12:20:52 -0400

    Completed the ROT13 Ceasar Cipher challenge.

--- JavaScript Algorithms and Data Structures/ROT13.js
+++ JavaScript Algorithms and Data Structures/ROT13.js
@@ -1,27 +1,18 @@
 function rot13(str) {
-    let returnStr = "";
-    const strArr = str.split(" ");
-
     // A is 65 in ASCII, Z is 90
-    strArr.forEach(word => {
-            returnStr += word.reduce((str, char) => {
-                const oldCode = char.charCodeAt();
-                if (oldCode < 65 || oldCode > 90) {
-                    str += String.fromCharCode(oldCode);
-                    return str;
-                } else {
-                    let newCode = oldCode - 13;
-                    if (newCode < 65) {
-                        newCode = 91 - (65 - newCode);  // Wrap letter back to Z if newCode is "less than" A
-                    }
-                    str += String.fromCharCode(newCode);
-                    return str;
-                }
-            }, "") + " ";
-        }
-    );
+    return str.split("").map(char => {
+        const oldCode = char.charCodeAt();
 
-    return returnStr.trim();
+        if (oldCode < 65 || oldCode > 90) {
+            return String.fromCharCode(oldCode);
+        } else {
+            let newCode = oldCode - 13;
+            if (newCode < 65) {
+                newCode = 91 - (65 - newCode);  // Wrap letter back to Z if newCode is "less than" A
+            }
+            return String.fromCharCode(newCode);
+        }
+    }).join("");
 }
 
 const testStr = "SERR PBQR PNZC";
