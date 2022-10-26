73ee436d9cd0c57026e71230137bf5f55d81c660
Christopher Nielson <cnielson3@gmail.com>
2022-10-26 12:04:58 -0400

    Added markdown-previewer project.

---
+++ Front End Development Projects/markdown-previewer/src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import { useState } from 'react';
+import { marked } from 'marked';
+
+const App = () => {
+	const [markdown, setMarkdown] = useState(
+		'# Welcome to my React Markdown Previewer!\n' +
+		'\n' +
+		'## This is a sub-heading...\n' +
+		'### And here\'s some other cool stuff:\n' +
+		'\n' +
+		'Heres some code, `<div></div>`, between 2 backticks.\n' +
+		'\n' +
+		'```\n' +
+		'// this is multi-line code:\n' +
+		'\n' +
+		'function anotherExample(firstLine, lastLine) {\n' +
+		'  if (firstLine == \'```\' && lastLine == \'```\') {\n' +
+		'    return multiLineCode;\n' +
+		'  }\n' +
+		'}\n' +
+		'```\n' +
+		'\n' +
+		'You can also make text **bold**... whoa!\n' +
+		'Or _italic_.\n' +
+		'Or... wait for it... **_both!_**\n' +
+		'And feel free to go crazy ~~crossing stuff out~~.\n' +
+		'\n' +
+		'There\'s also [links](https://www.freecodecamp.org), and\n' +
+		'> Block Quotes!\n' +
+		'\n' +
+		'And if you want to get really crazy, even tables:\n' +
+		'\n' +
+		'Wild Header | Crazy Header | Another Header?\n' +
+		'------------ | ------------- | -------------\n' +
+		'Your content can | be here, and it | can be here....\n' +
+		'And here. | Okay. | I think we get it.\n' +
+		'\n' +
+		'- And of course there are lists.\n' +
+		'  - Some are bulleted.\n' +
+		'     - With different indentation levels.\n' +
+		'        - That look like this.\n' +
+		'\n' +
+		'\n' +
+		'1. And there are numbered lists too.\n' +
+		'1. Use just 1s if you want!\n' +
+		'1. And last but not least, let\'s not forget embedded images:\n' +
+		'\n' +
+		'![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n' +
+		'');
+
+	const OnEditorChange = event => {
+		setMarkdown(marked.parse(event.target.value));
+	};
+
+  return (
+	  <div id='App'>
+		  <div id='editor' className='App-div'>
+			<textarea id='editor' defaultValue={markdown} onChange={OnEditorChange} />
+		  </div>
+
+		  <div id='preview' className='App-div'>
+			  {markdown}
+		  </div>
+	  </div>
+  );
+};
+
+export default App;
